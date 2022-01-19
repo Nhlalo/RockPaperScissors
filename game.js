80ade6c9e1800b78155db689478790271e41f871
Nhlalo <nhlalonkosi@gmail.com>
2022-01-19 16:01:34 +0200

    Add the playRound function

--- game.js
+++ game.js
@@ -5,3 +5,28 @@ function computerPlay(){
   return randomOption
 }
 
+function playRound(playerSelection, computerSelection){
+  
+
+  switch(true){
+   case playerSelection == 'scissors' && computerSelection == 'paper':
+   case playerSelection == 'rock' && computerSelection == 'scissors':
+   case playerSelection == 'paper' && computerSelection == 'rock':
+   console.log('you win');
+   break;
+   case playerSelection == 'rock' && computerSelection == 'paper':
+   case playerSelection == 'scissors' && computerSelection == 'rock':
+   case playerSelection == 'paper' && computerSelection == 'scissors':
+   console.log('you lose');
+   break;
+   case playerSelection == computerSelection:
+   console.log('draw');
+   case playerSelection =='' || computerSelection == '':
+   console.log(null);
+  }
+}
+
+
+
+
+
